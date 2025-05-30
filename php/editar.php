997359fa45918c3ddc4f3cdd1918a2cc878fcc3d
NICOLAS ARRIETA <nicolas.arieta712@gmail.com>
2025-05-29 22:56:45 -0500

    correccion de estilos

--- php/editar.php
+++ php/editar.php
@@ -9,7 +9,7 @@ $objconsulta = new cCliente;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="../Style/editar.css" />
+     <link rel="stylesheet" href="../css/Style/editar.css" />
     <title>X-RAY DIAGNOSTIC</title>
 </head>
 
--- php/editar2.php
+++ php/editar2.php
@@ -24,7 +24,7 @@ $row = mysqli_fetch_array($resultado)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="../Style/editar2.css" />
+     <link rel="stylesheet" href="../css/Style/editar2.css" />
     <title>X-RAY DIAGNOSTIC</title>
 </head>
 <style>
